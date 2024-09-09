1d3402432e4c170477e622cc4cac22318f04629f
jopaleti <oopaletijohnson@gmail.com>
2024-09-09 05:31:03 +0100

    updated

---
+++ hardhat.config.js
@@ -0,0 +1,61 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+// require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require("solidity-coverage");
+require("dotenv").config();
+require("hardhat-gas-reporter");
+require("hardhat-deploy");
+
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL || "https://www.sepolia.com/example";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0XKEY";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      url: "https://www.sepolia.com/example",
+      chainId: 1337,
+    },
+  },
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.8.24" }],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    token: "SEPOLIA",
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["OurToken"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet, it will take the first account as deployer.
+      // Note though that depending on how hardhat network are configured, the account 0 on one network can be different to on another
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
+};
---
+++ helper-hardhat-config.js
@@ -0,0 +1,19 @@
+const networkConfig = {
+  31337: {
+    name: "localhost",
+  },
+  11155111: {
+    name: "sepolia",
+    ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+const INITIAL_SUPPLY = "1000000000000000000000000";
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+  INITIAL_SUPPLY,
+};
